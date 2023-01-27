5ada1646d746c6f838626602311e1fe85a9a6b27
Manan Sonani <manan.s@biztechcs.com>
2023-01-27 12:32:49 +0530

    #comment feature: Added assignment 1

---
+++ src/app/components/success-alert/success-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styleUrls: ['./success-alert.component.scss']
+})
+export class SuccessAlertComponent implements OnInit{
+
+  ngOnInit(): void {
+    $('.success-alert').css('color', 'green');
+  }
+}
---
+++ src/app/components/warning-alert/warning-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.scss']
+})
+export class WarningAlertComponent implements OnInit{
+  
+  ngOnInit(): void {
+    $('.warning-alert').css('color', 'red');
+  }
+}
