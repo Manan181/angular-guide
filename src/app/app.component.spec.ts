d71d2547255ab4236e262493923f17c555a9523f
Manan Sonani <manan.s@biztechcs.com>
2023-01-27 14:41:28 +0530

    Initial setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-eslint'`, () => {
+  it(`should have as title 'angular-guide'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-eslint');
+    expect(app.title).toEqual('angular-guide');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-eslint app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-guide app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SuccessAlertComponent } from './components/success-alert/success-alert.component';
-import { WarningAlertComponent } from './components/warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
